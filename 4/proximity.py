588c530c99f7d51b1dd3b1d2072572faf2ca305c
Nimesh Priyodit <spam.me.bich@outlook.com>
2021-09-25 21:23:45 +0530

    code refactored

--- 4/proximity.py
+++ 4/proximity.py
@@ -6,9 +6,44 @@ sleep_time = 0.5
 led_pin = 12
 ir_pin = 16
 
+ir_key = 'IR'
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led_pin, GPIO.OUT)
 GPIO.setup(ir_pin, GPIO.IN)
+
+
+def main():
+	try:
+		while True:
+			ir_output = GPIO.input(ir_pin)
+			ir_status = 'Object detected'
+
+			if ir_output:
+				ir_status = 'Surroundings clear'
+
+			print(f"IR Sensor: {ir_status}")
+
+			sensor_data = {ir_key:ir_output}
+			
+			output = compute_led_intensity(sensor_data)			
+
+			if ir_output:
+				led_blink(sleep_time)
+			else:
+				GPIO.output(led_pin, True)
+				time.sleep(sleep_time)
+
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+
+def compute_led_intensity(inputs):
+	ir_output = not inputs[ir_key]
+
+	return ir_output
+
+
 
 def led_blink(sleep_time):
 	GPIO.output(led_pin, True)
@@ -16,13 +51,6 @@ def led_blink(sleep_time):
 	GPIO.output(led_pin, False)
 	time.sleep(sleep_time)
 
-try:
-	while True:
-		if GPIO.input(ir_pin):
-			led_blink(sleep_time)
-		else:
-			GPIO.output(led_pin, True)
-			time.sleep(sleep_time)
 
-except KeyboardInterrupt:
-	GPIO.cleanup()
+
+main()
