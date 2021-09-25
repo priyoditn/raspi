c67571cac4ed1c3c4cbe1db075b25e0046c95a75
Nimesh Priyodit <spam.me.bich@outlook.com>
2021-09-26 01:46:46 +0530

    running ultrasonic sensor. for now it gives boolean output

--- 4/distance.py
+++ 4/distance.py
@@ -1,23 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 
-sleep_time = 0.5
+sleep_time_high = 0.5
+sleep_time_low = 0.2
 
 led_pin = 12
 ir_pin = 16
 ultrasonic_trig_pin = 38
-ultrasonic_echo_pin = 40
+ultrasonic_echo_pin = 37
 
 ir_key = 'IR'
 ultrasonic_key = 'Ultrasonic'
 half_of_speed_of_sound_const = 34300 / 2
-far_away_threshold = 10000
+far_away_threshold = 200
+sensor_stabilise_time = 0.5
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led_pin, GPIO.OUT)
 GPIO.setup(ir_pin, GPIO.IN)
 GPIO.setup(ultrasonic_echo_pin, GPIO.IN)
 GPIO.setup(ultrasonic_trig_pin, GPIO.OUT)
+
+
+
 
 
 def main():
@@ -39,32 +44,37 @@ def main():
 			print(f"IR Sensor: {ir_status}\tUltrasonic Sensor: {ultrasonic_data}\tOutput: {output}")
 
 			if output:
-				led_blink(sleep_time)
+				led_blink()
 			else:
 				GPIO.output(led_pin, True)
-				time.sleep(sleep_time)
+				time.sleep(sleep_time_high)
 
-	except KeyboardInterrupt:
+	except:
 		GPIO.cleanup()
 
 
 
+
 def get_distance():
+	#	Initialise distance and pin
+	distance = -1
+	GPIO.output(ultrasonic_trig_pin, False)
+	time.sleep(sensor_stabilise_time)
+	
 	GPIO.output(ultrasonic_trig_pin, True)
 	time.sleep(0.00001)
 	GPIO.output(ultrasonic_trig_pin, False)
 	
-	object_found = False
-	time_taken = time.time()
+	while GPIO.input(ultrasonic_echo_pin) == 0:
+		t_init = time.time()
 	
-	if GPIO.input(ultrasonic_echo_pin):
-		time_taken = time.time() - time_taken
-		object_found = True
+	while GPIO.input(ultrasonic_echo_pin) == 1:
+		t_final = time.time()
+		distance = 0
 	
-	if object_found:
-		distance = time_taken * half_of_speed_of_sound_const
-	else:
-		distance = far_away_threshold
+	if distance == 0:
+		time_taken = t_final - t_init
+		distance = round(time_taken * 171500, 2)
 	
 	return distance
 
@@ -78,7 +88,7 @@ def compute_led_intensity(inputs):
 
 	# we require boolean for now
 	if ultrasonic_key in inputs:
-		inputs[ultrasonic_key] = inputs[ultrasonic_key] < far_away_threshold
+		inputs[ultrasonic_key] = inputs[ultrasonic_key] <= far_away_threshold
 
 	result = call_model(inputs)
 
@@ -88,20 +98,20 @@ def call_model(inputs):
 	if ultrasonic_key not in inputs:
 		inputs[ultrasonic_key] = False
 
-	if ir_key in inputs:
+	if ir_key not in inputs:
 			inputs[ir_key] = False
 
-	rule_output = inputs[ultrasonic_key] or inputs[ir_key]
+	rule_output = (inputs[ultrasonic_key] or inputs[ir_key])
 
 	return rule_output
 
 
 
-def led_blink(sleep_time):
+def led_blink():
 	GPIO.output(led_pin, True)
-	time.sleep(sleep_time)
+	time.sleep(sleep_time_high)
 	GPIO.output(led_pin, False)
-	time.sleep(sleep_time)
+	time.sleep(sleep_time_low)
 
 
 
